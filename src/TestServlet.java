344240ccb8f686dc04402ec18d3cbe72c7ffe6c3
tsubatyama <tsubasagolf1@gmail.com>
2018-07-24 17:51:09 +0900

    Test変更

--- src/TestServlet.java
+++ src/TestServlet.java
@@ -29,21 +29,6 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		//文字化け対策
-				request.setCharacterEncoding("utf-8");
-
-				String id = request.getParameter("id");
-				String password = request.getParameter("password");
-
-				if(id.equals("aa") && password.equals("aa")) {
-					request.setAttribute("result", "true");
-				}
-				else {
-					request.setAttribute("result", "false");
-				}
-
-				RequestDispatcher rd = request.getRequestDispatcher("LoginJson.jsp");
-				rd.forward(request, response);
 	}
 
 	/**
