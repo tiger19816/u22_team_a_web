0325da5f227f1a88e784b402bdcacce926e33cc8
tsubatyama <tsubasagolf1@gmail.com>
2018-08-04 22:05:52 +0900

    DataAccess

---
+++ src/DAO/DataAccess.java
@@ -0,0 +1,16 @@
+package DAO;
+
+import java.sql.SQLException;
+
+public class DataAccess extends Dao {
+
+	/**
+	 * コンストラクタ。
+	 *
+	 * @throws Exception 全ての例外。
+	 */
+	public DataAccess() throws Exception, SQLException {
+		super();
+	}
+
+}
