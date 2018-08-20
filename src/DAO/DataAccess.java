5cefd306e6dee15267fb4db8470167dca122aab7
tsubatyama <tsubasagolf1@gmail.com>
2018-08-20 10:30:05 +0900

    #5 comp (#13)
    * 一旦完成
    
    * ittan
    
    * k

--- src/DAO/DataAccess.java
+++ src/DAO/DataAccess.java
@@ -1,11 +1,17 @@
 package DAO;
 
 import java.sql.SQLException;
+import java.util.ArrayList;
 
+import entities.Posts;
 import hal.u22.works.team.a.NewProjectPostsConfirmationScreenActivityInfo.NewProjectPostsConfirmationScreenActivityInfo;
 
-public class DataAccess extends Dao {
 
+public class DataAccess extends Dao {
+	/**
+	 * フィールド。
+	 */
+	private String _sql;
 	/**
 	 * コンストラクタ。
 	 *
@@ -15,6 +21,63 @@ public class DataAccess extends Dao {
 		super();
 	}
 
+	//自分の投稿の情報を抽出
+	public ArrayList<Posts> MyPostSelect(int no) throws Exception, SQLException {
+		ArrayList<Posts> result = new ArrayList<Posts>();
+		try {
+			this._sql = "SELECT no,title, photo, post_money,post_date,cleaning_flag ";
+			this._sql += "FROM posts WHERE member_no = "+no+" ";
+			this._sql += "ORDER BY post_date desc";
+			rs = st.executeQuery(this._sql);
+			Posts p = null;
+			while(rs.next()) {
+				p = new Posts();
+				p.setNo( rs.getInt("no") );
+				p.setTitle( rs.getString("title") );
+				p.setPhoto( rs.getString("photo") );
+				p.setMoney( rs.getInt("post_money") );
+				p.setDate( rs.getString("post_date") );
+				p.setStatus( rs.getInt("cleaning_flag") );
+				result.add(p);
+			}
+			return result;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+            throw e;
+		}
+	}
+	//自分の投稿の情報を抽出
+	public ArrayList<Posts> MyAssistSelect(int no) throws Exception, SQLException {
+		ArrayList<Posts> result = new ArrayList<Posts>();
+		try {
+			this._sql = "Select p.no,p.title,p.photo,a.assist_money,a.assist_date,p.cleaning_flag ";
+			this._sql += "From posts p ";
+			this._sql += "RIGHT JOIN assists a ON a.post_no = p.no ";
+			this._sql += "WHERE a.member_no = "+no+" ";
+			this._sql += "ORDER BY a.assist_date desc";
+			rs = st.executeQuery(this._sql);
+			Posts p = null;
+			while(rs.next()) {
+				p = new Posts();
+				p.setNo( rs.getInt("p.no") );
+				p.setTitle( rs.getString("p.title") );
+				p.setPhoto( rs.getString("p.photo") );
+				p.setMoney( rs.getInt("a.assist_money") );
+				p.setDate( rs.getString("a.assist_date") );
+				p.setStatus( rs.getInt("p.cleaning_flag") );
+				result.add(p);
+			}
+			return result;
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+            throw e;
+		}
+	}
+	
+	
+}
 	public void InsertPosts(NewProjectPostsConfirmationScreenActivityInfo info) throws Exception, SQLException{
 		String sql = "insert into posts(member_no, category_no,post_date, post_money, place, latitude, longitude, title, content, photo, target_money, cleaning_flag) values("
 				+ " '" + info.getMemberNo() +"',"	//メンバーID			o
--- src/JoinProjectServlet.java
+++ src/JoinProjectServlet.java
@@ -1,11 +1,18 @@
 
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import DAO.DataAccess;
+import entities.Posts;
 
 /**
  * Servlet implementation class JoinProjectServlet
@@ -27,6 +34,37 @@ public class JoinProjectServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
+		//会員NO取得
+		String memberNo = "";
+		int judge = Integer.parseInt(request.getParameter("flag"));
+		System.out.println(memberNo);
+		memberNo = "3";
+		//投稿情報を格納する配列
+		ArrayList<Posts> posts = new ArrayList<Posts>();
+		//DBに接続
+		DataAccess da = null;
+		try {
+			da = new DataAccess();
+			//投稿情報抽出
+			if(judge == 1) {
+				posts = da.MyPostSelect(Integer.parseInt(memberNo));
+			}else {
+				posts = da.MyAssistSelect(Integer.parseInt(memberNo));
+			}
+			da.close();
+		}
+		catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+		catch (Exception e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+		}
+
+		request.setAttribute("LIST", posts);
+		RequestDispatcher rd = request.getRequestDispatcher("myposts_list_json.jsp");
+		rd.forward(request, response);
 	}
 
 	/**
