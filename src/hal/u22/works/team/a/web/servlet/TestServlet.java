d431dabe3838325157ac72c0b71f147c0db23739
tiger19816 <tiger19816@gmail.com>
2018-08-24 16:19:46 +0900

    Merge branch 'Login' into link_test

--- src/hal/u22/works/team/a/web/servlet/TestServlet.java
+++ src/hal/u22/works/team/a/web/servlet/TestServlet.java
@@ -1,6 +1,7 @@
 package hal.u22.works.team.a.web.servlet;
 
 import java.io.IOException;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -13,7 +14,6 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/TestServlet")
 public class TestServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
     /**
      * @see HttpServlet#HttpServlet()
      */
@@ -27,7 +27,6 @@ public class TestServlet extends HttpServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
 	/**
