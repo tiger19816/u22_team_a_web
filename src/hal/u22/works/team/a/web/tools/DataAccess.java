c054fbfe6722b07d513768d51fca49d18e290334
WIN10OHS60340\ohs60340 <aaa@bbb.com>
2018-08-29 20:49:33 +0900

    新規プロジェクトInsert部分修正

--- src/hal/u22/works/team/a/web/tools/DataAccess.java
+++ src/hal/u22/works/team/a/web/tools/DataAccess.java
@@ -21,21 +21,25 @@ public class DataAccess extends Dao {
 		super();
 	}
 
-	public void InsertPosts(NewProjectPostsConfirmationScreenActivityInfo info) throws Exception, SQLException{
+	public void InsertPosts(NewProjectPostsConfirmationScreenActivityInfo info, int Money) throws Exception, SQLException{
 		String sql = "insert into posts(member_no, category_no,post_date, post_money, place, latitude, longitude, title, content, photo, target_money, cleaning_flag) values("
-				+ " '" + info.getMemberNo() +"',"	//メンバーID			o
-				+ " '" + info.getCategoryNo() +"'," //カテゴリーNO			o
-				+ " cast( now() as date),"			//プロジェクト日			o
-				+ " '" + info.getPostMoney() +"',"  //プロジェクト初期投資	o
-				+ " '" + info.getPlace() +"',"		//場所				o
-				+ " '" + info.getLatitude() +"',"	//緯度				o
-				+ " '" + info.getLongitude() +"',"	//経度				o
-				+ " '" + info.getTitle() +"',"		//タイトル				o
-				+ " '" + info.getContent() +"',"	//内容				o
-				+ " '" + info.getPhoto() +"',"		//写真名				o
-				+ " 0,"								//収益				o
-				+ " 0"								//クリーン確認 初期値０	o
-				+ ")";
+				+ " '" + info.getMemberNo() +"',"	//メンバーID			
+				+ " '" + info.getCategoryNo() +"'," //カテゴリーNO			
+				+ " cast( now() as date),"			//プロジェクト日			
+				+ " '" + info.getPostMoney() +"',"  //プロジェクト初期投資	
+				+ " '" + info.getPlace() +"',"		//場所				
+				+ " '" + info.getLatitude() +"',"	//緯度				
+				+ " '" + info.getLongitude() +"',"	//経度				
+				+ " '" + info.getTitle() +"',"		//タイトル				
+				+ " '" + info.getContent() +"',"	//内容				
+				+ " '" + info.getPhoto() +"',"		//写真名				
+				+ " 0,";							//収益				
+		if(Integer.parseInt(info.getPostMoney()) < Money) {			//初期投資金額が初期目標金額より多かった場合
+				sql +=" 0";							//クリーン確認 	
+		}else {		
+				sql +=" 1";							
+		}	
+				sql += ")";
 
 		try {
 			if(!info.getTitle().equals("")) {
@@ -49,7 +53,7 @@ public class DataAccess extends Dao {
 			// TODO: handle exception
 		}
 	}
-
+	
 	//自分の投稿の情報を抽出
 	public ArrayList<Posts> MyPostSelect(int no) throws Exception, SQLException {
 		ArrayList<Posts> result = new ArrayList<Posts>();
